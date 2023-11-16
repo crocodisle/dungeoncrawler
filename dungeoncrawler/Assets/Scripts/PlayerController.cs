70ee4329620b90884a111ccf04bd702ee1ab5714
crocodisle <crocodisle@gmail.com>
2023-11-16 10:00:34 -0800

    Added new game objects and a test scene in PatrickScene.

--- dungeoncrawler/Assets/Scripts/PlayerController.cs
+++ dungeoncrawler/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5f;
+    public int facingDirection = 0; //0 = North, 1 = East, 2 = South, 3 = West
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
+        }
+    }
+
 }
