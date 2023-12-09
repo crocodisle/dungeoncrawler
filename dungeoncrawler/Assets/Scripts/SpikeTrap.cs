f3afc50ea01f5de0f37ab49df2934145ea422b92
crocodisle <crocodisle@gmail.com>
2023-12-08 16:39:28 -0800

    Fully implemented the spike trap.

--- dungeoncrawler/Assets/Scripts/SpikeTrap.cs
+++ dungeoncrawler/Assets/Scripts/SpikeTrap.cs
@@ -10,6 +10,15 @@ using UnityEngine;
 
 public class SpikeTrap : MonoBehaviour
 {
+    public GameObject spikeHome;
+    public GameObject[] targets;
+    public float speed = 20f;
+
+    public bool triggered = false;
+    public Vector3 currentTarget;
+    public bool moving = false;
+    public bool returning = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +28,57 @@ public class SpikeTrap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (moving)
+        {
+            transform.position += Vector3.Normalize(currentTarget - transform.position) * Time.deltaTime * speed;
+
+            if (Vector3.Distance(transform.position, currentTarget) < .2f)
+            {
+                moving = false;
+                returning = true;
+            }
+        }
+        if (returning)
+        {
+            transform.position += Vector3.Normalize (spikeHome.transform.position - transform.position) * Time.deltaTime * (speed/3);
+
+            if (Vector3.Distance(transform.position, spikeHome.transform.position) < .1f)
+            {
+                returning = false;
+                triggered = false;
+            }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!triggered)
+        {
+            RaycastHit hit;
+            foreach (GameObject target in targets)
+            {
+                if (Physics.Raycast(transform.position, target.transform.position - transform.position, out hit, Vector3.Distance(transform.position, target.transform.position)))
+                {
+                    Debug.Log("Checking " + target);
+                    Debug.Log(Vector3.Distance(transform.position, target.transform.position));
+                    if (hit.transform.gameObject.tag == "Player")
+                    {
+                        Debug.Log("Triggering!");
+                        currentTarget = target.transform.position;
+                        moving = true;
+                        triggered = true;
+                    }
+                }
+            }
+        }
         
     }
+
+
 }
